f3f670215f7bc510ffb971f23143c382f5181961
vsdiaman <vitor.diamantino8@gmail.com>
2023-04-06 23:00:35 -0300

    feat: component login, falta register e fazer a logica

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,16 +3,24 @@ import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope, faUnlockKeyhole } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './login/login.component.html',
-  styleUrls: ['./login/login.component.css'],
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
 })
-export class AppComponent implements OnInit {
+export class LoginComponent implements OnInit {
   MyfaLinkedin = faLinkedin;
   MyfaEnvelope = faEnvelope;
   MyfaUnlockKeyhole = faUnlockKeyhole;
 
+  public username: string = '';
+  password: string = '';
+  constructor() {
+    this.username = 'username teste';
+    this.password = 'senha teste';
+  }
   ngOnInit() {
     // Aqui você pode adicionar a lógica para autenticar o usuário
+    console.log('Username:', this.username);
+    console.log('Password:', this.password);
   }
 }
